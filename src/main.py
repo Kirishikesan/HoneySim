948430e0dee361a673db210cc0d0778150d66a29
unknown <gayanjayakody13@gmail.com>
2021-08-21 20:30:11 +0530

    Update main.py
    remove WDSSocket instances creating and add WSDmodbus instance

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from WDSSocket import WDSSocket
+from WDSmodbus import ModbusServer
 
 #Device Import script should be run to create relevant devices integrated with the modbus server
 
 
-wdsSocket=WDSSocket()
-wdsSocket.listen()
+wdsModbus=ModbusServer("",502)
+wdsModbus.run()
+wdsModbus.update(3,0,[54,88])
+print(wdsModbus.get(3,0,5))
 
 
