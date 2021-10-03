c4e8e4b0fb56e0894a16c808fec737ac80d4d419
Kirishikesan Kannan <kirishikesan.17@cse.mrt.ac.lk>
2021-10-03 18:03:48 +0530

    Changed gasConcentration sensor

--- src/models/sensor/gasConcentrationSensor.py
+++ src/models/sensor/gasConcentrationSensor.py
@@ -16,7 +16,7 @@ class GasConcentrationSensor(BaseSensor):
 
     def updateRegisters(self):
         value=[]
-        value.append(int(self.getValue()))
+        value.append(int(self.getValue()*100))
         self._device.updateToRegisters(self._register,self._address,value)
 
     def getValue(self):
